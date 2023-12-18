7d177d3d7eb8b39ffb120a58535cb344bb056aef
Marek Harrison <marekdharrison@gmail.com>
2023-12-18 12:47:50 -0500

    created top header and added logo

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
-
+    <header>
+        <div class="header-top">
+            <div class="logo">
+                <img src="./assets/AIS_LOGO.png" />
+            </div>
+        </div>
+    </header>
 </template>
 
 <script setup>
@@ -7,5 +13,20 @@
 </script>
 
 <style scoped>
+    header{
+        background-color: #375BDC;
+        width: 100%;
+        outline-style: solid;
+        outline-width: 5px;
+        outline-color: #375BDC;
+    }
 
+    header .header-top{
+        text-align: center;
+    }
+
+    header .header-top .logo img{
+        width: 200px;
+        height: auto;
+    }
 </style>
