939e013d158286054a6be2167b44f73b84b02327
Marek Harrison <marekdharrison@gmail.com>
2023-12-21 13:19:42 -0500

    created arrays for the 'frameworks' and 'technologies' elements and integrted them into the 'About' component

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,7 +2,24 @@
     <h1>Testing #4</h1>
 
     <div class="container">
+        <h2>Programming Languages</h2>
         <div class="wrapper" v-for="x in proglangs">
+            <img v-bind:src="x.url" />
+            <h4>{{ x.name }}</h4>
+        </div>
+    </div>
+
+    <div class="container">
+        <h2>Frameworks</h2>
+        <div class="wrapper" v-for="x in frameworks">
+            <img v-bind:src="x.url" />
+            <h4>{{ x.name }}</h4>
+        </div>
+    </div>
+
+    <div class="container">
+        <h2>Technologies</h2>
+        <div class="wrapper" v-for="x in technologies">
             <img v-bind:src="x.url" />
             <h4>{{ x.name }}</h4>
         </div>
@@ -20,6 +37,20 @@
     import css_icon from '@/assets/Icons/css.png'
     import sql_icon from '@/assets/Icons/sql.png'
 
+    import spring_icon from '@/assets/Icons/spring.png'
+    import django_icon from '@/assets/Icons/django.png'
+    import laravel_icon from '@/assets/Icons/laravel.png'
+    import vue_icon from '@/assets/Icons/VueJS.png'
+
+    import aws_icon from '@/assets/Icons/AWS.png'
+    import azure_icon from '@/assets/Icons/Azure.png'
+    import docean_icon from '@/assets/Icons/DigitalOcean.png'
+    import docker_icon from '@/assets/Icons/Docker.png'
+    import nginx_icon from '@/assets/Icons/Nginx.png'
+    import mysql_icon from '@/assets/Icons/mysql.png'
+    import sqlite_icon from '@/assets/Icons/SQLite.png'
+    import git_icon from '@/assets/Icons/git.png'
+
     const proglangs = reactive([
         {name: 'Java', url: jv_icon},
         {name: 'Python', url: py_icon},
@@ -29,11 +60,30 @@
         {name: 'CSS', url: css_icon},
         {name: 'SQL', url: sql_icon},
     ])
+
+    const frameworks = reactive([
+        {name: 'Spring', url: spring_icon},
+        {name: 'Django', url: django_icon},
+        {name: 'Laravel', url: laravel_icon},
+        {name: 'VueJS', url: vue_icon},
+    ])
+
+    const technologies = reactive([
+        {name: 'AWS', url: aws_icon},
+        {name: 'Azure', url: azure_icon},
+        {name: 'DigitalOcean', url: docean_icon},
+        {name: 'Docker', url: docker_icon},
+        {name: 'Nginx', url: nginx_icon},
+        {name: 'MySQL', url: mysql_icon},
+        {name: 'SQLite', url: sqlite_icon},
+        {name: 'Git', url: git_icon},
+    ])
 </script>
 
 <style scoped>
     .container{
         border-top: thin solid black;
+        margin: 20px;
     }
 
     .wrapper {
@@ -41,7 +91,7 @@
         border-radius: 10px;
         border: solid black 2px;
         margin: 10px;
-        padding: 20px 10px 0 10px;
+        padding: 20px 10px 10px 10px;
     }
 
     img{
